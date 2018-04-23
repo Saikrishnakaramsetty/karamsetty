a1b7b625ca0e5fd075935032393d2d57e2009208
Saikrishna <Raghavendra.Saikrishna@Standardbank.co.za>
2018-04-23 09:47:23 +0200

    branch changed

--- Example1.java
+++ Example1.java
@@ -4,6 +4,6 @@ public static void main(String[] args){
 System.out.println("******** Welcome to DevOps*****");
 System.out.println("******** changed now *****");
 System.out.println("******** check out explanation *****");
-
+System.out.println("******** checking branch explanation*****");
 }
 }
